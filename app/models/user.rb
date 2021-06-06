f7357226831f86d1e9f12c4d690a6f315c792863
damari <motokin128@gmail.com>
2021-06-06 12:57:26 +0900

    ゲストログイン処理を実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def self.guest
+    # メールアドレスが"guesut@example.com"のユーザーがいればそれを取得、
+    # 存在しなければブロック内の情報を持つユーザーを取得。
+    find_or_create_by!(email: 'guest@example.com') do |user|
+      # パスワードをランダムで生成
+      user.password = SecureRamndom.urlsafe_base64
+      # user.confirmed_at = Time.now  # Confirmable を使用している場合は必要
+      # 例えば name を入力必須としているならば， user.name = "ゲスト" なども必要
+    end
+  end
 end
