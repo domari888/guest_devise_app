342762237005dbd4b654b7e375c54762d8ccc4de
damari <motokin128@gmail.com>
2021-06-06 13:16:41 +0900

    ゲストログイン用ボタンを実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
     # 存在しなければブロック内の情報を持つユーザーを取得。
     find_or_create_by!(email: 'guest@example.com') do |user|
       # パスワードをランダムで生成
-      user.password = SecureRamndom.urlsafe_base64
+      user.password = SecureRandom.urlsafe_base64
       # user.confirmed_at = Time.now  # Confirmable を使用している場合は必要
       # 例えば name を入力必須としているならば， user.name = "ゲスト" なども必要
     end
